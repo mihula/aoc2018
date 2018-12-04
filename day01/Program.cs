9906e67b589c9df3c00c199b752f97ef38ae0cc4
René Mihula <mihula@provys.com>
2018-12-04 20:17:04 +0100

    inicializace

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace day01
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("Hello World!");
         }
     }
 }
